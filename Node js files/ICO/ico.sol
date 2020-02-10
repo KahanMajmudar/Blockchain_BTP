5688f607f39581823653db8df9ebb45623a473ce
Kahan Majmudar <kahan.m.btechi16@ahduni.edu.in>
2020-02-10 18:51:18 +0530

    added solhint

--- Node js files/ICO/ico.sol
+++ Node js files/ICO/ico.sol
@@ -1,23 +1,24 @@
 pragma solidity ^0.5.8;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
+import "./node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./node_modules/@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "./node_modules/@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
 
-import "../node_modules/@openzeppelin/contracts/crowdsale/Crowdsale.sol";
-import "../node_modules/@openzeppelin/contracts/crowdsale/validation/CappedCrowdsale.sol";
-import "../node_modules/@openzeppelin/contracts/crowdsale/validation/TimedCrowdsale.sol";
-import "../node_modules/@openzeppelin/contracts/ownership/Ownable.sol";
-import "../node_modules/@openzeppelin/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
-import "../node_modules/@openzeppelin/contracts/crowdsale/emission/AllowanceCrowdsale.sol";
+import "./node_modules/@openzeppelin/contracts/crowdsale/Crowdsale.sol";
+import "./node_modules/@openzeppelin/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "./node_modules/@openzeppelin/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "./node_modules/@openzeppelin/contracts/ownership/Ownable.sol";
+import "./node_modules/@openzeppelin/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
+import "./node_modules/@openzeppelin/contracts/crowdsale/emission/AllowanceCrowdsale.sol";
 
 
-contract MyToken is ERC20, ERC20Detailed, ERC20Pausable{
+contract MyToken is ERC20, ERC20Detailed, ERC20Pausable {
 
     constructor(string memory name, string memory symbol, uint8 decimals, uint ICO_SUPP)
-    ERC20Detailed(name, symbol, decimals) public{
+    public ERC20Detailed(name, symbol, decimals) {
 
+        //solhint-disable-next-line mark-callable-contracts
         _mint(msg.sender, ICO_SUPP);
 
     }
@@ -25,34 +26,28 @@ contract MyToken is ERC20, ERC20Detailed, ERC20Pausable{
 }
 
 
-contract MyICO is Ownable, Crowdsale, CappedCrowdsale, TimedCrowdsale, RefundablePostDeliveryCrowdsale, AllowanceCrowdsale{
+contract MyICO is Ownable, Crowdsale, CappedCrowdsale, TimedCrowdsale,
+RefundablePostDeliveryCrowdsale, AllowanceCrowdsale {
 
-    uint ourEndTime;
-    uint ourStartTime;
-    uint ourRate;
+    uint internal ourEndTime;
+    uint internal ourStartTime;
+    uint internal ourRate;
     // uint ourLock = now+30*1 days;
 
 
     // enum CrowdsaleStage { PreICO, ICO, Round1, Round2, Round3, SaleEnd }
     // CrowdsaleStage public stage = CrowdsaleStage.PreICO;
 
-    uint stage = 0;
+    uint internal stage = 0;
 
     constructor(uint256 rate, address payable wallet, IERC20 token, address tokenOwner,
-    uint softcap,  uint hardcap, uint startTime, uint endTime)
+    uint softcap, uint hardcap, uint startTime, uint endTime)
 
-    Crowdsale(rate, wallet, token)
+    public Crowdsale(rate, wallet, token)
     AllowanceCrowdsale(tokenOwner)
     CappedCrowdsale(hardcap)
     RefundableCrowdsale(softcap)
-    TimedCrowdsale(startTime, endTime) public{
-
-        //address deployed_address = MyToken.getAddress();
-        //require(address(msg.sender) == address(deployed_address), "Owner of Token can create ICO");
-
-        require(rate > 0, 'Rate cannot be 0');
-        require(wallet != address(0), 'Wallet address cannot be 0');
-        require(address(token) != address(0), 'Token address cannot be 0');
+    TimedCrowdsale(startTime, endTime) {
 
         ourRate = rate;
         ourStartTime = startTime;
@@ -60,30 +55,30 @@ contract MyICO is Ownable, Crowdsale, CappedCrowdsale, TimedCrowdsale, Refundabl
         applyBonus();
     }
 
-    modifier beforeEnd(){
-
+    modifier beforeEnd() {
+        //solhint-disable-next-line not-rely-on-time
         require(now <= ourEndTime, "CrowdSale has Ended");
         _;
 
     }
 
-    modifier afterStart(){
-
+    modifier afterStart() {
+        //solhint-disable-next-line not-rely-on-time
         require(now >= ourStartTime, "CrowdSale has not Started");
         _;
 
     }
 
 
-    // function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal view{
+    function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal view {
 
-    //     require(block.timestamp >= ourLock);
-    //     super._preValidatePurchase(_beneficiary, _weiAmount);
+        // require(block.timestamp >= ourLock);
+        super._preValidatePurchase(_beneficiary, _weiAmount);
 
-    // }
+    }
 
 
-    function changeRound() public onlyOwner() beforeEnd() afterStart(){
+    function changeRound() public onlyOwner() beforeEnd() afterStart() {
 
         stage = stage + 1;
         applyBonus();
@@ -91,16 +86,14 @@ contract MyICO is Ownable, Crowdsale, CappedCrowdsale, TimedCrowdsale, Refundabl
     }
 
 
-    function applyBonus() internal onlyOwner() beforeEnd(){
+    function applyBonus() internal onlyOwner() beforeEnd() {
 
-        require(stage < 6, 'Rounds Excedded!!');
+        require(stage < 6, "Rounds Excedded!!");
 
-        if(stage == 1){
+        if (stage == 1) {
             ourRate = ourRate*2;
 
-        }
-
-        else{
+        } else {
             ourRate = ourRate/2;
         }
 
@@ -116,3 +109,4 @@ contract MyICO is Ownable, Crowdsale, CappedCrowdsale, TimedCrowdsale, Refundabl
     }
 
 }
+
