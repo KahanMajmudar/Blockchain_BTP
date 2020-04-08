8fc591952d31a6e56231ed7276c46e038d68619a
Kahan Majmudar <kahan.m.btechi16@ahduni.edu.in>
2020-04-08 19:00:16 +0530

    testing area

--- Testground/test2.js
+++ Testground/test2.js
@@ -2,18 +2,30 @@ const Web3 = require('web3')
 
 const web3 = new Web3()
 
-const param1 = (web3.utils.sha3("onERC1155Received(address,address,uint256,uint256,bytes)"))
-const param2 = (web3.utils.sha3("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))
+// const param1 = (web3.utils.sha3("onERC1155Received(address,address,uint256,uint256,bytes)"))
+// const param2 = (web3.utils.sha3("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))
 
-console.log(param1)
-console.log(param2)
+// console.log(param1)
+// console.log(param2)
 
-// console.log(param3)
+// // console.log(param3)
 
-// console.log( web3.utils.bytesToHex( web3.utils.hexToBytes(param3).slice(0,4) ) )
+// // console.log( web3.utils.bytesToHex( web3.utils.hexToBytes(param3).slice(0,4) ) )
 
-console.log(process.env.NODE_ENV)
+// console.log(process.env.NODE_ENV)
 
-import _ from 'loadash'
+// import _ from 'loadash'
 
-console.log(_)
+// console.log(_)
+
+const origin = '0xdD870fA1b7C4700F2BD7f44238821C26f7392148'
+// let smth = new Uint16Array(10)
+// smth[0] = origin
+// console.log(smth[0])
+
+
+// console.log(web3.utils.padLeft(web3.utils.hexToBytes(origin, 32)))
+// dD870fA1b7C4700F2BD7f44238821C26f739
+// 000000000000000000000000000000000000
+
+
